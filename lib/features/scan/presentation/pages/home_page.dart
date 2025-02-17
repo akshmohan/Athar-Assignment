9b30e899252de8ef25f36383b9bd73fc0ae01412
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-02-17 12:09:43 +0530

    Added bloc and the camera opens up but the rest doesnt work

--- lib/features/scan/presentation/pages/home_page.dart
+++ lib/features/scan/presentation/pages/home_page.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: avoid_print
+
+import 'package:athar_assignment/features/scan/presentation/cubit/expiry_date_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,9 +12,28 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text("Scan")),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Scan Expiry Date'),
+        child: BlocConsumer<ExpiryDateCubit, ExpiryDateState>(
+          listener: (context, state) {
+            if(state is ExpiryDateLoaded) {
+                 
+            } else if(state is ExpiryDateError) {
+                 print('state is ExpiryDateError');
+            }
+          },
+          builder: (context, state) {
+            return ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+              ),
+              onPressed: () {
+                context.read<ExpiryDateCubit>().pickAndProcessImage();
+              },
+              child: const Text(
+                'Scan Expiry Date',
+                style: TextStyle(color: Colors.white),
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:athar_assignment/features/scan/domain/services/image_picker_service.dart';
+import 'package:athar_assignment/features/scan/domain/services/ocr_service.dart';
+import 'package:athar_assignment/features/scan/presentation/cubit/expiry_date_cubit.dart';
 import 'package:athar_assignment/features/scan/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:  false,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
+      home: BlocProvider(
+        create: (context) => ExpiryDateCubit(
+          ImagePickerService(),
+          OcrService(),
+        ),
+        child: const HomePage(),
+      ),
     );
   }
 }

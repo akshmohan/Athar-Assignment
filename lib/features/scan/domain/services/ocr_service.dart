e312d92e498619aa4fe7abe363706fb860aee7fc
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-02-17 10:43:29 +0530

    Created the OCR function

---
+++ lib/features/scan/domain/services/ocr_service.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+class OcrService {
+  final TextRecognizer _textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+
+  Future<String?> extractExpiryDate(File imageFile) async {
+  try {
+     final InputImage inputImage = InputImage.fromFile(imageFile);
+
+   final RecognizedText recognizedText = await _textRecognizer.processImage(inputImage);
+
+   final text = recognizedText.text;
+
+   if(text.isEmpty) {
+     return null;
+   }
+
+   return _extractExpiryDateFromText(text);
+  } catch (e) {
+    print('--------------ERROR DURING OCR: $e--------------------------');
+    return null;
+  }
+  
+  }
+
+  String? _extractExpiryDateFromText(String text) {
+
+    final expiryDatePattern = RegExp(r'(\d{1,2}/\d{1,2}/\d{4}|\d{1,2}-\d{1,2}-\d{4}|\d{1,2}\s+[a-zA-Z]+\s+\d{4})');
+    final match = expiryDatePattern.firstMatch(text);
+
+    return match?.group(0);
+  }
+ }
